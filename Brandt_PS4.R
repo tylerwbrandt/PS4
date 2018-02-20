a22d807034a3bd60fef77098d3b69611b6418fc9
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-19 23:37:33 -0600

    Edit Indenting

--- Brandt_PS4.R
+++ Brandt_PS4.R
@@ -3,10 +3,11 @@
 # Getting Started
 
 myFunction<-function(doorthing, doorthing2, x){
-doorthing1<-doorthing2<-sample(1:3, 1)
-if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
-x
+  doorthing1<-doorthing2<-sample(1:3, 1)
+  if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
+    x
 }
+
 myFunction(sample(1:3, 1), sample(1:3, 1))
 # Should return a TRUE if these samples are equal and
 # a false if they are not
