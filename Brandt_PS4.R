5122219bca9cad1785d2a6275d2bd8bb55c5b9c1
Tyler Brandt <tylerbrandt02@gmail.com>
2018-02-19 23:36:49 -0600

    Paste Getting Started
    Copied and pasted getting started code

--- Brandt_PS4.R
+++ Brandt_PS4.R
@@ -1,2 +1,13 @@
 ## Tyler Brandt, PS 4
 
+# Getting Started
+
+myFunction<-function(doorthing, doorthing2, x){
+doorthing1<-doorthing2<-sample(1:3, 1)
+if (doorthing1==doorthing2){ x<-TRUE } else { x==FALSE }
+x
+}
+myFunction(sample(1:3, 1), sample(1:3, 1))
+# Should return a TRUE if these samples are equal and
+# a false if they are not
+
